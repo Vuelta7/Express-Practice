3c36300a76a09bccd0bf37e36a6cdac915002f39
vuelta7 <markvuelta07@example.com>
2025-05-30 00:52:00 +0800

    update: Finish Chapter 2.2

---
+++ chapter_2.2/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+data = ["gehlee"];
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send(`
+    <body>
+        <p>
+            ${JSON.stringify(data)}
+        </p>
+    </body>
+    `);
+});
+
+app.get("/dashboard", (req, res) => {
+  console.log("hi", req.method);
+  res.status(300).send("<i>hello</i>");
+});
+
+app.get("/api/data", (req, res) => {
+  res.send(data);
+});
+
+app.post("/api/data", (req, res) => {
+  const newEntry = req.body;
+  console.log(newEntry);
+  data.push(newEntry.name);
+  console.log(data);
+  res.sendStatus(201);
+});
+
+app.delete("api/endpoint", (req, res) => {
+  data.pop();
+  console.log("deleted");
+  console.log(data);
+  res.sendStatus(203);
+});
+
+app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));
